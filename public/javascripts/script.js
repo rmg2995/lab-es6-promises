1604b6b022910a99e240e4f8ded41d38e8ec15ee
Roy Gutierrez <roymg29@gmail.com>
2020-07-21 11:25:55 -0400

    almost done-

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,23 +1,35 @@
 // This will print in the wrong order
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
-for (let i = 0; i < steak.length; i++) {
-  addFood(steak[i], '#steak');
-}
+// for (let i = 0; i < steak.length; i++) {
+//   addFood(steak[i], "#steak");
+// }
 
 // Iteration 1 using callbacks
-addFood(steak[0], '#steak', () => {
-  // ... your code here
+addFood(steak[0], "#steak", () => {
+  addFood(steak[1], "#steak", () => {
+    addFood(steak[2], "#steak", () => {
+      addFood(steak[3], "#steak", () => {
+        addFood(steak[4], "#steak", () => {
+          addFood(steak[5], "#steak", () => {
+            addFood(steak[6], "#steak", () => {
+              addFood(steak[7], "#steak", () => {});
+            });
+          });
+        });
+      });
+    });
+  }); // ... your code here
 });
 
 // Iteration 2 using `.then()`
-addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
+addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
   // ... your code here
 });
 
 // Iteration 3 using async and await
 
-brusselSprouts.forEach(eachStep => {
+brusselSprouts.forEach((eachStep) => {
   async function makeFood(step) {
     // ... your code here
   }
